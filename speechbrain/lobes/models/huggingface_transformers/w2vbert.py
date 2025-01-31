b1da55fa9ddb69acd1dedd60214280def598a326
Jesujoba Alabi <jalabi@ws803lx.lsv.uni-saarland.de>
2025-01-31 10:46:06 +0100

    implementation for w2vbert

--- speechbrain/lobes/models/huggingface_transformers/w2vbert.py
+++ speechbrain/lobes/models/huggingface_transformers/w2vbert.py
@@ -181,11 +181,10 @@ class Wav2VecBert(HFTransformersInterface):
         # wav_cpu = wav_norm.cpu()  # Ensure it's on the CPU
         # wav_npy = wav.numpy()  # Convert to NumPy array
         print("hidden_states before shape:", wav.shape)
+        print("wavlen = ", wav_lens)
         wavx = self.feature_extractor(wav.cpu().numpy())
         # print("the features from seamless = ", wavx)
-        wav = torch.tensor(wavx["input_features"], device=self.model.device)
-        print("the device1 = ", self.model.device)
-        print("the device2 = ", self.device)
+        wav = torch.tensor(wavx["input_features"], device=self.model.device) # device
 
         print("hidden_states shape:", wav.shape)
         print("attention_mask shape:", padding_mask.shape)
